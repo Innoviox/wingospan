a4fdb299806310ff7bd2cef9b7c3e54f25d6abb8
Simon Chervenak <sarras305@gmail.com>
2020-12-31 14:37:54 -0500

    Update player.go

--- player.go
+++ player.go
@@ -7,11 +7,23 @@ type Player struct {
 	bonus []Bonus
 }
 
-func (p *Player) playBird(g *Game, b Bird, r Region, f []Food, e EggPayment) {
+func (p *Player) playBird(b Bird, r Region, f []Food, e EggPayment) {
 	p.payFood(f)
 	p.payEggs(e)
 
 	p.discard(b)
 
 	p.board.playBird(b, r)
+}
+
+func (p *Player) gainFood(g *Game, food []Food, discard *Bird) {
+	for _, f := range food {
+		p.birdfeeder(g, f)
+	}
+
+	if discard != nil {
+		p.discard(discard)
+	}
+
+	p.activate(g, Forest)
 }
