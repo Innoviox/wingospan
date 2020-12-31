898a8e8264c650acfaf7ed31a1e866294d41e1ce
Simon Chervenak <sarras305@gmail.com>
2020-12-31 10:47:53 -0500

    all action strings work

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
 func main() {
 	game := new(Game)
@@ -8,34 +12,30 @@ func main() {
 	// readAction birds
 	game.deck = make([]Bird, 0)
 
-	//file, _ := os.Open("parse/birds.csv")
-	//r := csv.NewReader(file)
-	//
-	//records, _ := r.ReadAll()
-	//
-	//for i, line := range records {
-	//	if i == 0 {
-	//		continue
-	//	}
-	//
-	//	game.deck = append(game.deck, Bird {
-	//		name:     line[0],
-	//		region:   parseRegion(line[1]),
-	//		cost:     readCost(line[2]),
-	//		points:   Atoi(line[3]),
-	//		nest:     Nest(Atoi(line[4])),
-	//		eggLimit: Atoi(line[5]),
-	//		eggs:     0,
-	//		wingspan: Atoi(line[6]),
-	//		action:   readAction(line[8]),
-	//	})
-	//}
+	file, _ := os.Open("parse/birds.csv")
+	r := csv.NewReader(file)
 
-	//for _, bird := range game.deck {
-	//	fmt.Println(game.deck[13].action)
-	//}
+	records, _ := r.ReadAll()
 
-	q := readAction("A,VF")
-	fmt.Println(q.cause.things[0])
-	fmt.Println(q)
+	for i, line := range records {
+		if i == 0 {
+			continue
+		}
+
+		game.deck = append(game.deck, Bird {
+			name:     line[0],
+			region:   parseRegion(line[1]),
+			cost:     readCost(line[2]),
+			points:   Atoi(line[3]),
+			nest:     Nest(Atoi(line[4])),
+			eggLimit: Atoi(line[5]),
+			eggs:     0,
+			wingspan: Atoi(line[6]),
+			action:   readAction(line[8]),
+		})
+	}
+
+	for _, bird := range game.deck {
+		fmt.Println(bird.action)
+	}
 }
