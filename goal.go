1552dd2f12b78ea225cac013747937f764c18994
Simon Chervenak <sarras305@gmail.com>
2021-01-02 11:15:43 -0500

    add simple goal methods

--- goal.go
+++ goal.go
@@ -1,5 +1,78 @@
 package main
 
-type Goal struct {
+type GoalDef func (*Player) int
 
+type Goal struct {
+	sides [2]GoalDef
+	chosen int
+}
+
+func eggsInRegion(region Region) GoalDef {
+	return func (p *Player) int {
+		total := 0
+
+		for _, bird := range p.board.rows[region] {
+			total += bird.eggs
+		}
+
+		return total
+	}
+}
+
+func birdsInRegion(region Region) GoalDef {
+	return func (p *Player) int {
+		total := 0
+
+		for _ := range p.board.rows[region] {
+			total++
+		}
+
+		return total
+	}
+}
+
+func eggsInNest(nest Nest) GoalDef {
+	return func (p *Player) int {
+		total := 0
+
+		for _, row := range p.board.rows {
+			for _, bird := range row {
+				if bird.nest == nest {
+					total += bird.eggs
+				}
+			}
+		}
+
+		return total
+	}
+}
+
+func nestsWithEggs(nest Nest) GoalDef {
+	return func (p *Player) int {
+		total := 0
+
+		for _, row := range p.board.rows {
+			for _, bird := range row {
+				if bird.nest == nest {
+					total++
+				}
+			}
+		}
+
+		return total
+	}
+}
+
+func totalBirds() GoalDef {
+	return func (p *Player) int {
+		total := 0
+
+		for _, row := range p.board.rows {
+			for _ := range row {
+				total++
+			}
+		}
+
+		return total
+	}
 }
