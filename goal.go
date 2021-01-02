ce8ae971b99682b65d3614cfb6585b019338b99a
Simon Chervenak <sarras305@gmail.com>
2021-01-02 17:50:30 -0500

    go is weird

--- goal.go
+++ goal.go
@@ -41,7 +41,7 @@ func birdsInRegion(region Region) GoalDef {
 	return func (p *Player) int {
 		total := 0
 
-		for _ = range p.board.rows[region] {
+		for range p.board.rows[region] {
 			total++
 		}
 
@@ -86,7 +86,7 @@ func totalBirds() GoalDef {
 		total := 0
 
 		for _, row := range p.board.rows {
-			for _ = range row {
+			for range row {
 				total++
 			}
 		}
