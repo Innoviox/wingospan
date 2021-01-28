3be3cf40f2fffab27ee992fe93d4c44d1a01a462
Simon Chervenak <sarras305@gmail.com>
2021-01-27 20:46:28 -0500

    player clone

--- cloning.go
+++ cloning.go
@@ -8,7 +8,30 @@ func (g *Game) clone() *Game {
 }
 
 func (p *Player) clone() *Player {
-	
+	other := new(Player)
+	other.board = p.board.clone()
+
+	other.food = map[Food]int {}
+	for i := 0; i < 5; i++ {
+		other.food[Food(i)] = p.food[Food(i)]
+	}
+
+	other.hand = make([]Bird, len(p.hand))
+	for i := 0; i < len(p.hand); i++ {
+		other.hand[i] = p.hand[i].clone()
+	}
+
+	// todo bonus
+
+	return other
+}
+
+func (b *Board) clone() *Board {
+
+}
+
+func (b Bird) clone() Bird {
+
 }
 
 func (g *Game) load(other *Game) {
