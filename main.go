2e6ab148a927f12e714bd7cff6ce60cdfb7ff94e
Simon Chervenak <sarras305@gmail.com>
2021-01-27 21:38:31 -0500

    starting testing :)

--- main.go
+++ main.go
@@ -20,20 +20,7 @@ func main() {
 	for _, b := range p.hand {
 		fmt.Println(b.name, b.cost)
 	}
+	fmt.Println(p.food)
 
-	for _, m := range p.generateMoves(game) {
-		if m.t == PlayBird {
-			fmt.Println(m.a.b, m.a.r, m.a.f)
-			m.f(m.a)
-			break
-		}
-	}
-
-	for _, m := range p.generateMoves(game) {
-		if m.t == GainFood {
-			fmt.Println(m.a.e)
-			m.f(m.a)
-			break
-		}
-	}
+	fmt.Println(p.chooseMove(game))
 }
