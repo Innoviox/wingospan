f67a76d56f94b5700a3bf9ada9172d1f706a1609
Simon Chervenak <sarras305@gmail.com>
2021-01-27 21:33:30 -0500

    maximax

--- ai.go
+++ ai.go
@@ -3,3 +3,35 @@ package main
 func (p *Player) choosePregame() {
 
 }
+
+func (p *Player) chooseMove(g *Game) Move {
+	m, _ = p.maximax(g, 0, 3)
+	return m
+}
+
+func (p *Player) maximax(g *Game, ply int, maxply int) (Move, int) {
+	bestScore := 0
+	var bestMove Move
+
+	state := g.clone()
+
+	for _, m := range p.generateMoves(g) {
+		g.doMove(m)
+
+		var score int
+		if ply < maxply {
+			_, score = p.maximax(g, ply + 1, maxply)
+		} else {
+			score = p.board.rawScore()
+		}
+
+		if score > bestScore {
+			bestScore = score
+			bestMove = m
+		}
+
+		g.load(state)
+	}
+
+	return (bestMove, bestScore)
+}
